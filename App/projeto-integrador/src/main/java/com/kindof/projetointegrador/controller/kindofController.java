f0f46faf2e970194e8c72a88288cc0841749856e
Fernanda Ramos <fee_salles@hotmail.com>
2020-10-18 16:31:30 -0300

    spring boot

---
+++ App/projeto-integrador/src/main/java/com/kindof/projetointegrador/controller/kindofController.java
@@ -0,0 +1,10 @@
+package com.kindof.projetointegrador.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+@RequestMapping("/kindofproject")
+public class kindofController {
+
+}
