dfbc5e2ee60a53f3da93d2b4ed99b67fb7ab3909
Fernanda Ramos <fee_salles@hotmail.com>
2020-10-18 18:21:25 -0300

    atualizacao controller

--- App/projeto-integrador/src/main/java/com/kindof/projetointegrador/controller/kindofController.java
+++ App/projeto-integrador/src/main/java/com/kindof/projetointegrador/controller/kindofController.java
@@ -1,10 +1,22 @@
 package com.kindof.projetointegrador.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController 
-@RequestMapping("/kindofproject")
 public class kindofController {
+	 
+@RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+
+@RequestMapping("/rewards")
+	public String rewards(){
+    return "rewards";
+}
 
 }
