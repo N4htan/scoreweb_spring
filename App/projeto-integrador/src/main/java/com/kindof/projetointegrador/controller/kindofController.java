c45b1d40c27157a814644b0beaa8ef8fc6b0ab68
Fernanda Ramos <fee_salles@hotmail.com>
2020-10-18 18:38:10 -0300

    controller

--- App/projeto-integrador/src/main/java/com/kindof/projetointegrador/controller/kindofController.java
+++ App/projeto-integrador/src/main/java/com/kindof/projetointegrador/controller/kindofController.java
@@ -1,21 +1,22 @@
 package com.kindof.projetointegrador.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController 
+@Controller
 public class kindofController {
-	 
+
 @RequestMapping("/index")
     public String index(){
-        return "index";
-    }
+
+    return "index";
+}
 
 
 @RequestMapping("/rewards")
-	public String rewards(){
+    public String rewards(){
+
     return "rewards";
 }
 
